0f04e68b1b93fd7fb1301cefbe2669a413a3183c
Zokirjon Mamadjonov <zmamadjonov@freeportmetrics.com>
2023-06-24 15:45:07 +0200

    completed home fragment logic

---
+++ app/src/main/java/com/zikrandmehr/firebaseandroid/landing_page/home/HomeAdapter.kt
@@ -0,0 +1,42 @@
+package com.zikrandmehr.firebaseandroid.landing_page.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.zikrandmehr.firebaseandroid.databinding.HomeViewHolderBinding
+
+class HomeItem(
+    val imageView: Int,
+    val title: CharSequence,
+    val description: CharSequence,
+    val onClick: () -> Unit
+)
+
+class HomeAdapter(val items: List<HomeItem>) : RecyclerView.Adapter<HomeAdapter.HomeViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = HomeViewHolderBinding.inflate(inflater, parent, false)
+        return HomeViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount() = items.count()
+
+    inner class HomeViewHolder(
+        private val binding: HomeViewHolderBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: HomeItem) {
+            binding.apply {
+                ivFunction.setImageResource(item.imageView)
+                tvTitle.text = item.title
+                tvDescription.text = item.description
+                root.setOnClickListener { item.onClick.invoke() }
+            }
+        }
+    }
+}
--- app/src/main/java/com/zikrandmehr/firebaseandroid/landing_page/home/HomeFragment.kt
+++ app/src/main/java/com/zikrandmehr/firebaseandroid/landing_page/home/HomeFragment.kt
@@ -5,56 +5,93 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.zikrandmehr.firebaseandroid.R
+import com.zikrandmehr.firebaseandroid.databinding.FragmentHomeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setStatusBarColor()
+        setupViews()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setStatusBarColor() {
+        val window = requireActivity().window
+        val windowColor = ContextCompat.getColor(requireContext(), R.color.firebase_orange)
+        window.statusBarColor = windowColor
+        window.navigationBarColor = windowColor
+    }
+
+    private fun setupViews() {
+        val adapter = HomeAdapter(getHomeItems())
+        binding.rvHome.adapter = adapter
+    }
+
+    private fun getHomeItems() = listOf(
+        HomeItem(
+            imageView = R.drawable.fb_authentication,
+            title = getText(R.string.auth_title),
+            description = getText(R.string.auth_desc),
+            onClick = { navigateToAuthenticationFragment() }
+        ),
+        HomeItem(
+            imageView = R.drawable.fb_realtime_database,
+            title = getText(R.string.realtime_database_title),
+            description = getText(R.string.realtime_database_desc),
+            onClick = { navigateToAuthenticationFragment() }
+        ),
+        HomeItem(
+            imageView = R.drawable.fb_cloud_firestore,
+            title = getText(R.string.cloud_firestore_title),
+            description = getText(R.string.cloud_firestore_desc),
+            onClick = { navigateToAuthenticationFragment() }
+        ),
+        HomeItem(
+            imageView = R.drawable.fb_storage,
+            title = getText(R.string.storage_title),
+            description = getText(R.string.storage_desc),
+            onClick = { navigateToAuthenticationFragment() }
+        ),
+        HomeItem(
+            imageView = R.drawable.fb_messaging,
+            title = getText(R.string.messaging_title),
+            description = getText(R.string.messaging_desc),
+            onClick = { navigateToAuthenticationFragment() }
+        ),
+        HomeItem(
+            imageView = R.drawable.fb_crashlytics,
+            title = getText(R.string.crashlytics_title),
+            description = getText(R.string.crashlytics_desc),
+            onClick = { navigateToAuthenticationFragment() }
+        ),
+        HomeItem(
+            imageView = R.drawable.fb_remote_config,
+            title = getText(R.string.remote_config_title),
+            description = getText(R.string.remote_config_desc),
+            onClick = { navigateToAuthenticationFragment() }
+        )
+    )
+
+    private fun navigateToAuthenticationFragment() {
+        //TODO 
     }
 }
